ffad24dc0da0b5f2ddac9673e78ec116140fec2a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-27 20:10:13 +0100

    Added createTask function to add the inserted task.

--- 01-Pomodoro/js/main.js
+++ 01-Pomodoro/js/main.js
@@ -4,4 +4,17 @@ let timer = null;
 let timerBreak = null;
 let current = null;
 
+const bAdd = document.querySelector("#bAdd");
+const itTask = document.querySelector("#itTask");
+const form = document.querySelector("#form");
+
+const createTask = (value) => {
+  const newTask = {
+    id: (Math.random() * 100).toString(36).slice(3),
+    title: value,
+    completed: false,
+  };
+
+  tasks.unshift(newTask) // Agrega el objeto newTask al principio del array
+};
 
