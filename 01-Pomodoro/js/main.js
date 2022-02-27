58d145aaf99b046bc0ccae512175bd5ba3158845
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-27 20:28:35 +0100

    Added renderTasks function.

--- 01-Pomodoro/js/main.js
+++ 01-Pomodoro/js/main.js
@@ -15,14 +15,32 @@ const createTask = (value) => {
     completed: false,
   };
 
-  tasks.unshift(newTask) // Agrega el objeto newTask al principio del array
+  tasks.unshift(newTask); // Agrega el objeto newTask al principio del array
 };
 
+const renderTasks = () => {
+  const html = tasks.map((task) => {
+    return `
+            <div class="task">
+                <div class="completed">${
+                  task.completed
+                    ? `<span class="done">Done</span>`
+                    : `<button class="start-button" data-id="${task.id}>Start</button>`
+                }</div>
+                <div class="title">${task.title}</div>
+            </div>
+        `;
+  }); // Array de strings con la estrucutra html
+
+  const tasksContainer = document.querySelector('#tasks')
+  tasksContainer.innerHTML = html.join(); //Unimos todos los strings del array por un espacio vacío
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (itTask.value !== "") {
     createTask(itTask.value);
     itTask.value = "";
+    renderTasks();
   }
 });
